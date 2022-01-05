52dabd8516c867d0cce5278ec1e9212db32a5e61
MARIA-SÂNZIANA PRICHICI <maria.prichici@stud.ubbcluj.ro>
2022-01-05 18:31:22 +0200

    Lab11

--- Prichici_Sanziana_Lab10/ListPage.xaml.cs
+++ Prichici_Sanziana_Lab10/ListPage.xaml.cs
@@ -30,5 +30,11 @@ namespace Prichici_Sanziana_Lab10
             await App.Database.DeleteShopListAsync(slist);
             await Navigation.PopAsync();
         }
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            var shopl = (ShopList)BindingContext;
+            listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+        }
     }
 }
